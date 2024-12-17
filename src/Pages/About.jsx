0a5c1eedacc5ab83f694a21019bb019d434a1c25
danger-3003 <naremsumanth@gmail.com>
2024-12-17 17:54:53 +0530

    Updated About Section

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,6 +5,7 @@ import Marquee from "react-fast-marquee";
 import Element from "../assets/MarqueeElement.svg";
 import Quotation from "../Components/Quotation";
 import Clients from "./Clients";
+import RoadMap from "../assets/About/roadMap.jpg";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -30,17 +31,17 @@ function About() {
                 className="bg-[#fff4e7] relative overflow-hidden py-14 flex items-center justify-center flex-col"
             >
                 <div className="w-[90vw] md:w-[45rem] lg:w-[58rem] xl:w-[70rem] flex items-center justify-between flex-col sm:flex-row gap-10">
-                    {/* <div data-aos="fade-right" className="flex items-center justify-center flex-col">
-                        <img src={Person} alt="Person" className="w-[70vw] sm:w-[15rem] md:w-[17rem] lg:w-[20rem]" />
+                    <div data-aos="fade-right" className="flex items-center justify-center flex-col">
+                        <img src={RoadMap} alt="Person" className="w-[70vw] sm:w-[15rem] md:w-[17rem] lg:w-[20rem]" />
                         <div className="flex items-center justify-center flex-col font-[Montserrat] mt-5">
-                            <p className="font-semibold text-xl bg-gradient-to-br from-BStart via-BMiddle to-BEnd text-transparent bg-clip-text">Sri Vallapani Sreenu</p>
-                            <p>(Managing Director)</p>
+                            <p className="font-semibold text-xl bg-gradient-to-br from-BStart via-BMiddle to-BEnd text-transparent bg-clip-text">Sri Sai Sarada</p>
+                            <p>(OnGoing Project)</p>
                         </div>
-                    </div> */}
+                    </div>
                     {/* basis-[65%] */}
                     <div
                         data-aos="fade-left"
-                        className=" flex items-center sm:items-center justify-center flex-col gap-5 md:gap-10"
+                        className=" flex items-center sm:items-center justify-center flex-col gap-5 md:gap-10 basis-[65%]"
                     >
                         <div>
                             <p className="text-center font-medium text-xl sm:text-2xl md:text-3xl font-[Montserrat] bg-gradient-to-r from-BStart via-BMiddle to-BEnd text-transparent bg-clip-text">
